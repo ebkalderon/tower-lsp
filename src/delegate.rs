a7918bcf906317dcfa862e6df1df9add2f77cc2b
Eyal Kalderon <ebkalderon@gmail.com>
2019-09-03 02:52:40 +0800

    Document internal not_initialized_error() function

--- src/delegate.rs
+++ src/delegate.rs
@@ -282,6 +282,10 @@ impl<T: LanguageServer> LanguageServerCore for Delegate<T> {
     }
 }
 
+/// Error response returned for every request received before the server is initialized.
+///
+/// See [here](https://microsoft.github.io/language-server-protocol/specification#initialize) for
+/// reference.
 fn not_initialized_error() -> Error {
     Error::new(ErrorCode::ServerError(-32002))
 }
