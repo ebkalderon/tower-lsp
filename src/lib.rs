d7c713394786321792162e6ea67f94b4a056b34a
Eyal Kalderon <ebkalderon@gmail.com>
2019-09-04 20:17:09 +0800

    Re-export Incoming message type

--- src/lib.rs
+++ src/lib.rs
@@ -76,6 +76,7 @@
 pub extern crate lsp_types;
 
 pub use self::delegate::{MessageStream, Printer};
+pub use self::message::Incoming;
 pub use self::service::{ExitReceiver, ExitedError, LspService};
 pub use self::stdio::Server;
 
