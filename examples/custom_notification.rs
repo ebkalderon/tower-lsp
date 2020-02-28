941e9a7363903afebb89489ef4d30ffe20cf9744
Eyal Kalderon <ebkalderon@gmail.com>
2020-02-28 19:53:34 +0800

    Remove ExitReceiver and LspService::close_handle()
    A nice side effect of the previous `poll_ready()` changes: since the
    `Server` now shuts itself automatically as soon as the underlying
    service returns a `Poll::Ready(Err(_))`, there is no reason to keep
    `ExitReceiver` around anymore.
    
    This allows users to await the `Server::serve()` future directly, which
    improves the API ergonomics quite a bit and allows us to get rid of
    `ExitReceiver` entirely.

--- examples/custom_notification.rs
+++ examples/custom_notification.rs
@@ -103,10 +103,8 @@ async fn main() {
     let stdout = tokio::io::stdout();
 
     let (service, messages) = LspService::new(Backend::default());
-    let handle = service.close_handle();
-    let server = Server::new(stdin, stdout)
+    Server::new(stdin, stdout)
         .interleave(messages)
-        .serve(service);
-
-    handle.run_until_exit(server).await;
+        .serve(service)
+        .await;
 }
