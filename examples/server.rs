e9042d2467d3a7aa1e8ee6d93e13af6c4c358396
Eyal Kalderon <ebkalderon@gmail.com>
2020-07-23 16:48:38 +0800

    Update examples/server.rs to lsp-types 0.78

--- examples/server.rs
+++ examples/server.rs
@@ -15,7 +15,7 @@ impl LanguageServer for Backend {
                 text_document_sync: Some(TextDocumentSyncCapability::Kind(
                     TextDocumentSyncKind::Incremental,
                 )),
-                hover_provider: Some(true),
+                hover_provider: Some(HoverProviderCapability::Simple(true)),
                 completion_provider: Some(CompletionOptions {
                     resolve_provider: Some(false),
                     trigger_characters: Some(vec![".".to_string()]),
